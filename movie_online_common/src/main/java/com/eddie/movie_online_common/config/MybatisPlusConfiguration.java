51e8a02c893fb8e109c8d7a8973cae44b63cea38
eddie <eddie0yang@gmail.com>
2022-01-07 09:56:05 +0800

    mybatis plus + lambda接入

--- movie_online_common/src/main/java/com/eddie/movie_online_common/config/MybatisPlusConfiguration.java
+++ movie_online_common/src/main/java/com/eddie/movie_online_common/config/MybatisPlusConfiguration.java
@@ -1,21 +1,30 @@
-//package com.eddie.movie_online_common.config;
-//
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-///**
-// * 配置分页插件
-// *
-// */
-//@Configuration
-//public class MybatisPlusConfiguration {
-//
-//    /**
-//     * 分页插件
-//     */
-//    @Bean
-//    public PaginationInterceptor paginationInterceptor() {
-//        return new PaginationInterceptor();
-//    }
-//}
+package com.eddie.movie_online_common.config;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置分页插件
+ *
+ */
+@Configuration
+public class MybatisPlusConfiguration {
+
+    /**
+     * MyBatis-Plus插件配置
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
